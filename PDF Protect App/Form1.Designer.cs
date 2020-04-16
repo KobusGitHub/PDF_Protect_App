c0f24fdafb29eac61b003bb30f7bc3a199b187c8
Kobus Jonker <jacobusjonker@gmail.com>
2020-04-16 18:42:53 +0200

    Commit

---
+++ PDF Protect App/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace PDF_Protect_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFileSelected = new System.Windows.Forms.TextBox();
+            this.btnBrowseFile = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDestFolder = new System.Windows.Forms.Button();
+            this.txtDestFolder = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtOwnerPassword = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtOwnerPassword2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(231, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PDF Protect";
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.ForeColor = System.Drawing.Color.Maroon;
+            this.txtId.Location = new System.Drawing.Point(145, 24);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(289, 20);
+            this.txtId.TabIndex = 1;
+            this.txtId.Text = "ID#:  ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(21, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID Locator";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(21, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Email Locator";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.ForeColor = System.Drawing.Color.Maroon;
+            this.txtEmail.Location = new System.Drawing.Point(145, 61);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(289, 20);
+            this.txtEmail.TabIndex = 3;
+            this.txtEmail.Text = "Email:  ";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtFileSelected
+            // 
+            this.txtFileSelected.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFileSelected.ForeColor = System.Drawing.Color.Maroon;
+            this.txtFileSelected.Location = new System.Drawing.Point(145, 27);
+            this.txtFileSelected.Name = "txtFileSelected";
+            this.txtFileSelected.Size = new System.Drawing.Size(289, 20);
+            this.txtFileSelected.TabIndex = 5;
+            // 
+            // btnBrowseFile
+            // 
+            this.btnBrowseFile.BackColor = System.Drawing.Color.Maroon;
+            this.btnBrowseFile.ForeColor = System.Drawing.Color.White;
+            this.btnBrowseFile.Location = new System.Drawing.Point(440, 25);
+            this.btnBrowseFile.Name = "btnBrowseFile";
+            this.btnBrowseFile.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseFile.TabIndex = 6;
+            this.btnBrowseFile.Text = "Browse";
+            this.btnBrowseFile.UseVisualStyleBackColor = false;
+            this.btnBrowseFile.Click += new System.EventHandler(this.btnBrowseFile_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(17, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Source PDF File";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(17, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Destination Folder";
+            // 
+            // btnDestFolder
+            // 
+            this.btnDestFolder.BackColor = System.Drawing.Color.Maroon;
+            this.btnDestFolder.ForeColor = System.Drawing.Color.White;
+            this.btnDestFolder.Location = new System.Drawing.Point(440, 63);
+            this.btnDestFolder.Name = "btnDestFolder";
+            this.btnDestFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnDestFolder.TabIndex = 9;
+            this.btnDestFolder.Text = "Browse";
+            this.btnDestFolder.UseVisualStyleBackColor = false;
+            this.btnDestFolder.Click += new System.EventHandler(this.btnDestFolder_Click);
+            // 
+            // txtDestFolder
+            // 
+            this.txtDestFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDestFolder.ForeColor = System.Drawing.Color.Maroon;
+            this.txtDestFolder.Location = new System.Drawing.Point(145, 65);
+            this.txtDestFolder.Name = "txtDestFolder";
+            this.txtDestFolder.Size = new System.Drawing.Size(289, 20);
+            this.txtDestFolder.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(17, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Owner Password";
+            // 
+            // txtOwnerPassword
+            // 
+            this.txtOwnerPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOwnerPassword.ForeColor = System.Drawing.Color.Maroon;
+            this.txtOwnerPassword.Location = new System.Drawing.Point(145, 25);
+            this.txtOwnerPassword.Name = "txtOwnerPassword";
+            this.txtOwnerPassword.PasswordChar = '*';
+            this.txtOwnerPassword.Size = new System.Drawing.Size(289, 20);
+            this.txtOwnerPassword.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(17, 62);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Confirm Password";
+            // 
+            // txtOwnerPassword2
+            // 
+            this.txtOwnerPassword2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOwnerPassword2.ForeColor = System.Drawing.Color.Maroon;
+            this.txtOwnerPassword2.Location = new System.Drawing.Point(145, 62);
+            this.txtOwnerPassword2.Name = "txtOwnerPassword2";
+            this.txtOwnerPassword2.PasswordChar = '*';
+            this.txtOwnerPassword2.Size = new System.Drawing.Size(289, 20);
+            this.txtOwnerPassword2.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtOwnerPassword2);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtOwnerPassword);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(40, 310);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(540, 100);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Password";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtEmail);
+            this.groupBox2.Controls.Add(this.txtId);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(40, 185);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(540, 100);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Locators";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtDestFolder);
+            this.groupBox3.Controls.Add(this.txtFileSelected);
+            this.groupBox3.Controls.Add(this.btnBrowseFile);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.btnDestFolder);
+            this.groupBox3.Location = new System.Drawing.Point(40, 68);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(540, 100);
+            this.groupBox3.TabIndex = 17;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Files";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackColor = System.Drawing.Color.Maroon;
+            this.btnGenerate.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.ForeColor = System.Drawing.Color.White;
+            this.btnGenerate.Location = new System.Drawing.Point(185, 428);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(269, 51);
+            this.btnGenerate.TabIndex = 18;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(629, 505);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PDF Protect";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFileSelected;
+        private System.Windows.Forms.Button btnBrowseFile;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnDestFolder;
+        private System.Windows.Forms.TextBox txtDestFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtOwnerPassword;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtOwnerPassword2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ PDF Protect App/Form1.cs
@@ -0,0 +1,254 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Pdf.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDF_Protect_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            this.txtFileSelected.Text = openFileDialog1.FileName;
+
+        }
+
+      
+        private void btnDestFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            this.txtDestFolder.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            var filePath = Path.GetDirectoryName(this.txtFileSelected.Text) + "\\";
+
+            if (!File.Exists(this.txtFileSelected.Text))
+            {
+                MessageBox.Show("Invalid File");
+                return;
+            }
+
+            var destPath = this.txtDestFolder.Text + "\\";
+
+            if (!Directory.Exists(this.txtDestFolder.Text))
+            {
+                MessageBox.Show("Invalid Directory");
+                return;
+            }
+
+            var fileName = Path.GetFileName(this.txtFileSelected.Text);
+            var searchIdKey = txtId.Text;
+
+            if (string.IsNullOrEmpty(searchIdKey))
+            {
+                MessageBox.Show("Invalid ID Key");
+                return;
+            }
+
+            var searchNameKey = txtEmail.Text;
+
+            if (string.IsNullOrEmpty(searchNameKey))
+            {
+                MessageBox.Show("Invalid Name Key");
+                return;
+            }
+
+
+            if(txtOwnerPassword.Text != txtOwnerPassword2.Text || string.IsNullOrEmpty(txtOwnerPassword.Text))
+            {
+                MessageBox.Show("Password Invalid");
+                return;
+            }
+
+            // Delete all files in destination
+            DirectoryInfo di = new DirectoryInfo(destPath);
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+
+            
+            try
+            {
+                var result = this.SplitProtect(filePath, destPath, fileName, searchIdKey, searchNameKey, txtOwnerPassword.Text);
+                if (result == true)
+                {
+                    MessageBox.Show("Files created successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                using (StreamWriter w = File.AppendText(destPath + "\\log.txt"))
+                {
+                    w.WriteLine("Error");
+                    w.WriteLine(ex.Message);
+                }
+
+                MessageBox.Show("Files created failed");
+            } 
+
+            
+
+        }
+
+
+        private bool SplitProtect(string path, string destPath, string filename, string searchIdKey, string searchNameKey, string ownerPwd)
+        {
+
+            var identifiers = new List<Identifier>();
+
+            var source = Path.Combine(path, filename);
+
+            if (!File.Exists(source))
+            {
+                MessageBox.Show("File does not exist");
+                return false;
+            }
+            var dest = Path.Combine(destPath, filename);
+            File.Copy(source, dest, true);
+
+            // Open the file
+            // PdfDocument inputDocument = PdfReader.Open(filename, PdfDocumentOpenMode.Import);
+            PdfDocument inputDocument = PdfReader.Open(dest, PdfDocumentOpenMode.Import);
+
+            //string name = Path.GetFileNameWithoutExtension(filename);
+            string name = Path.GetFileNameWithoutExtension(dest);
+            for (int idx = 0; idx < inputDocument.PageCount; idx++)
+            {
+                // Create new document
+                PdfDocument outputDocument = new PdfDocument();
+                outputDocument.Version = inputDocument.Version;
+                outputDocument.Info.Title = String.Format("{0}_{1}.pdf", idx + 1, inputDocument.Info.Title);
+                outputDocument.Info.Creator = inputDocument.Info.Creator;
+
+                // Add the page and save it
+                var page = inputDocument.Pages[idx];
+
+                var identifier = getIdentifier(page, searchIdKey, searchNameKey);
+                if (identifier == null)
+                {
+                    MessageBox.Show("Search Values Invalid");
+
+                    return false;
+                }
+                identifiers.Add(identifier);
+
+                outputDocument.AddPage(page);
+
+                // var saveFileName = String.Format("{0}_{1}.pdf", name, idx + 1);
+                var saveFileName = String.Format("{0}.pdf", identifier.PersonName);
+
+                outputDocument.Save(destPath + "\\" + saveFileName);
+            }
+
+
+            using (StreamWriter w = File.AppendText(destPath + "\\log.txt"))
+            {
+                w.WriteLine("Owner Password: '" + ownerPwd + "'");
+                w.WriteLine("FileName,Password");
+
+                // Set Password
+                foreach (var identifier in identifiers)
+                {
+                    w.WriteLine(identifier.PersonName + ".pdf,'" + identifier.IdNumber + "'");
+                    ProtectFile(destPath + "\\" + identifier.PersonName + ".pdf", ownerPwd, identifier.IdNumber);
+                }
+
+            }
+
+            return true;
+        }
+
+        private Identifier getIdentifier(PdfPage page, string idKey, string nameKey)
+        {
+            Identifier identifier = new Identifier();
+
+            for (int index = 0; index < page.Contents.Elements.Count; index++)
+            {
+                PdfDictionary.PdfStream stream = page.Contents.Elements.GetDictionary(index).Stream;
+                var outputText = new PDFTextExtractor().ExtractTextFromPDFBytes(stream.Value);
+
+                var searchIdIndex = outputText.IndexOf(idKey);
+                if (searchIdIndex > -1)
+                {
+                    var id = outputText.Substring(searchIdIndex + idKey.Length, 13);
+                    identifier.IdNumber = id;
+                    if (identifier.isLoaded)
+                    {
+                        return identifier;
+                    }
+                }
+
+                var searchNameIndex = outputText.IndexOf(nameKey);
+                if (searchNameIndex > -1)
+                {
+                    var attIndex = outputText.IndexOf("@");
+
+                    var startIndex = searchNameIndex + nameKey.Count();
+                    var endIndex = attIndex - startIndex;
+                    var name = outputText.Substring(startIndex, endIndex);
+                    identifier.PersonName = name;
+                    if (identifier.isLoaded)
+                    {
+                        return identifier;
+                    }
+
+                }
+
+            }
+            return null;
+        }
+        private void ProtectFile(string filenameDest, string ownerPw, string userPw)
+        {
+
+            // Open an existing document. Providing an unrequired password is ignored.
+            PdfDocument document = PdfReader.Open(filenameDest, "some text");
+
+            PdfSecuritySettings securitySettings = document.SecuritySettings;
+
+            // Setting one of the passwords automatically sets the security level to 
+            // PdfDocumentSecurityLevel.Encrypted128Bit.
+            securitySettings.UserPassword = userPw;
+            securitySettings.OwnerPassword = ownerPw;
+
+            // Don't use 40 bit encryption unless needed for compatibility
+            //securitySettings.DocumentSecurityLevel = PdfDocumentSecurityLevel.Encrypted40Bit;
+
+            // Restrict some rights.
+            securitySettings.PermitAccessibilityExtractContent = false;
+            securitySettings.PermitAnnotations = false;
+            securitySettings.PermitAssembleDocument = false;
+            securitySettings.PermitExtractContent = false;
+            securitySettings.PermitFormsFill = true;
+            securitySettings.PermitFullQualityPrint = false;
+            securitySettings.PermitModifyDocument = false;
+            securitySettings.PermitPrint = true;
+
+
+            // Save the document...
+            document.Save(filenameDest);
+            // ...and start a viewer.
+            //Process.Start(filenameDest);
+        }
+
+
+
+    }
+}
